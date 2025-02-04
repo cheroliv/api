881adc24e25b521ddea017094fbd4836cf6951dc
cheroliv <cheroliv@laposte.net>
2025-02-04 05:04:00 +0100

    refactor: Update dependency resolution and plugin configuration in `settings.gradle.kts`
    This commit refactors the dependency resolution and plugin configuration within `settings.gradle.kts`. Key changes include:
    
    -   **Consolidation of Maven Repositories:** Replaced individual `maven()` calls with a `setOf()` to define the Maven repositories, then iterating over it with `forEach(::maven)` to define all Maven repositories at once. This makes the setup more organized.
    -   **Plugin Management:** Commented out some plugin declarations like `org.jbake.site`, `com.github.node-gradle.node` and the `org.asciidoctor` plugins, while keeping their versions defined in the extra properties. This indicates they are not immediately needed or used but retained for potential future use. The other plugins `kotlin` and `org.springframework.boot`, `io.spring.dependency-management` and  `com.github.ben-manes.versions` are kept active.
    -   **Repository Duplication:** In the `dependencyResolutionManagement` block, we now have the same set of repositories using the `setOf()` and `forEach(::maven)` combination. This change assures that both the plugin resolution and the dependency resolution are using the same set of repositories.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,18 +4,14 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven("https://plugins.gradle.org/m2/")
-        maven("https://maven.xillio.com/artifactory/libs-release/")
-        maven("https://mvnrepository.com/repos/springio-plugins-release")
-        maven("https://archiva-repository.apache.org/archiva/repository/public/")
+        setOf(
+            "https://plugins.gradle.org/m2/",
+            "https://mvnrepository.com/repos/springio-plugins-release",
+            "https://maven.xillio.com/artifactory/libs-release/",
+            "https://archiva-repository.apache.org/archiva/repository/public/",
+        ).forEach(::maven)
     }
     plugins {
-        id("org.jbake.site").version(extra["jbake-gradle.version"].toString())
-        id("com.github.node-gradle.node").version(extra["node-gradle.version"].toString())
-//        id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
-//        id("org.asciidoctor:asciidoctor-gradle-jvm-slides") version (extra["asciidoctor.gradle.version"].toString())
-//        id("org.asciidoctor:asciidoctor-gradle-base") version (extra["asciidoctor.gradle.version"].toString())
-//        id("org.asciidoctor:asciidoctor-gradle-jvm-gems") version (extra["asciidoctor.gradle.version"].toString())
         kotlin("jvm").version(extra["kotlin.version"].toString())
         kotlin("plugin.serialization").version(extra["kotlin.version"].toString())
         kotlin("plugin.allopen").version(extra["kotlin.version"].toString())
@@ -23,7 +19,13 @@ pluginManagement {
         kotlin("plugin.spring").version(extra["kotlin.version"].toString())
         id("org.springframework.boot").version(extra["springboot.version"].toString())
         id("io.spring.dependency-management").version(extra["spring_dependency_management.version"].toString())
-        id( "com.github.ben-manes.versions"). version( extra["deps.version"].toString())
+        id("com.github.ben-manes.versions").version(extra["deps.version"].toString())
+//        id("org.jbake.site").version(extra["jbake-gradle.version"].toString())
+//        id("com.github.node-gradle.node").version(extra["node-gradle.version"].toString())
+//        id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
+//        id("org.asciidoctor:asciidoctor-gradle-jvm-slides") version (extra["asciidoctor.gradle.version"].toString())
+//        id("org.asciidoctor:asciidoctor-gradle-base") version (extra["asciidoctor.gradle.version"].toString())
+//        id("org.asciidoctor:asciidoctor-gradle-jvm-gems") version (extra["asciidoctor.gradle.version"].toString())
     }
 }
 
@@ -33,10 +35,12 @@ dependencyResolutionManagement {
         mavenLocal()
         google()
         mavenCentral()
-        maven("https://maven.xillio.com/artifactory/libs-release/")
-        maven("https://mvnrepository.com/repos/springio-plugins-release")
-        maven("https://archiva-repository.apache.org/archiva/repository/public/")
-        maven ("https://plugins.gradle.org/m2/")
+        setOf(
+            "https://plugins.gradle.org/m2/",
+            "https://mvnrepository.com/repos/springio-plugins-release",
+            "https://maven.xillio.com/artifactory/libs-release/",
+            "https://archiva-repository.apache.org/archiva/repository/public/"
+        ).forEach(::maven)
     }
 }
 rootProject.name = "api"
