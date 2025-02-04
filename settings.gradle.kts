33a776f15eb5022de546eae19301f62ca589ab02
cheroliv <cheroliv@laposte.net>
2025-02-05 00:22:01 +0100

    refactor: Update Gradle plugins and dependencies management
    This commit updates the Gradle build configuration:
    
    -   Updates Kotlin plugins to use the `kotlin(...)` syntax with explicit versioning, ensuring consistency in plugin versions.
    - Updates spring-boot and dependency management plugins to use explicit versioning.
    -   Updates the dependency versions plugin to use an explicit version.
    -   Removes unnecessary commented-out code.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,25 +11,7 @@ pluginManagement {
             "https://archiva-repository.apache.org/archiva/repository/public/"
         ).forEach(::maven)
     }
-    plugins {
-//    id(libs.plugins.`kotlin-jvm`.get().id) version libs.versions.kotlin
-//    id(libs.plugins.`kotlin-spring`.get().id) version libs.versions.kotlin
-//    id(libs.plugins.`kotlin-allopen`.get().id) version libs.versions.kotlin
-//    id(libs.plugins.`kotlin-noarg`.get().id) version libs.versions.kotlin
-//    id(libs.plugins.`kotlin-serialization`.get().id) version libs.versions.kotlin
-//    id(libs.plugins.`spring-boot`.get().id) version libs.versions.springboot
-//    id(libs.plugins.`dependency-management`.get().id) version libs.versions.`dependency-management`
-//    id(libs.plugins.versions.get().id) version libs.versions.versions
-        kotlin("plugin.serialization").version(extra["kotlin.version"].toString())
-        kotlin("plugin.allopen").version(extra["kotlin.version"].toString())
-        kotlin("plugin.noarg").version(extra["kotlin.version"].toString())
-        kotlin("plugin.spring").version(extra["kotlin.version"].toString())
-        id("org.springframework.boot").version(extra["springboot.version"].toString())
-        id("io.spring.dependency-management").version(extra["spring_dependency_management.version"].toString())
-        id("com.github.ben-manes.versions").version(extra["deps.version"].toString())
-    }
 }
-
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
