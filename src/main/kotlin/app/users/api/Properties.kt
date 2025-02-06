edb71c17bf0f32ef13a53c63ba39b4e2ab738baa
cheroliv <cheroliv@laposte.net>
2025-02-06 19:37:02 +0100

    refactor: Move mail configuration to `MailBox` and simplify
    This commit refactors the mail configuration by moving the SMTP properties from a separate `Mail` class into the `MailBox` structure. This change consolidates all mail-related settings in one place.
    - Removed the `Mail` class.
    - Moved `SmtpProperty` to `MailProperties` inside `MailBox.MailAccount`.
    - Added `MailTransfer` class into `MailProperties`.
    - Added `Transport` class into `MailProperties`.
    - Renamed `smptProperties` to `properties` in `MailAccount`.
    - Removed `imapsProperties` in `MailAccount`.
    - Moved mail properties to `MailAccount` in `MailBox`.
    - Added default value to `MailAccount` port.
    - Added a `Constants` class for mail properties default values.

--- src/main/kotlin/app/users/api/Properties.kt
+++ src/main/kotlin/app/users/api/Properties.kt
@@ -1,5 +1,6 @@
 package app.users.api
 
+import app.users.api.Constants.SMTP
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.PropertySource
@@ -17,79 +18,34 @@ class Properties @ConstructorBinding constructor(
     val goVisitMessage: String,
     val clientApp: ClientApp = ClientApp(),
     val database: Database = Database(),
-    val mail: Mail = Mail(),
     val mailbox: MailBox = MailBox(),
     val http: Http = Http(),
     val cache: Cache = Cache(),
     val security: Security = Security(),
     val cors: CorsConfiguration = CorsConfiguration(),
 ) {
-    //    text_to_add="# google
-    //    app.mail.no-reply.host=smtp.gmail.com
-    //    app.mail.no-reply.port=587
-    //
-    //    # google account test
-    //    app.mail.no-reply.email=tester@gmail.com
-    //    app.mail.no-reply.password=sxckqebcmaimwfvl"
-    class Mail(
-        val name: String = "",
-        val token: String = "",
-        val enabled: Boolean = false,
-        val from: String = "",
-        val baseUrl: String = "",
-        val host: String = "",
-        val port: Int = -1,
-        val password: String = "",
-        val property: SmtpProperty = SmtpProperty(),
-    ) {
-        class SmtpProperty(
-            val debug: Boolean = false,
-            val transport: Transport = Transport(),
-            val smtp: Smtp = Smtp()
-        ) {
-            class Transport(val protocol: String = "")
-            class Smtp(
-                val auth: Boolean = false,
-                val starttls: Starttls = Starttls()
-            ) {
-                class Starttls(val enable: Boolean = false)
-            }
-        }
-    }
-
     class MailBox(
         val noReply: MailAccount = MailAccount(),
 //        val contact: Mail = Mail(),
-//        val recrutment: Mail = Mail(),
+//        val job: Mail = Mail(),
+//        val newsletter: Mail = Mail(),
     ) {
         class MailAccount(
+            val name: String = "",
             val from: String = "",
             val password: String = "",
-            val baseUrl: String = "",
-            val name: String = "no-reply",
             val host: String = "",
-            val port: Int = -1,
-            val enabled: Boolean = false,
-            val smptProperties: SmtpProperties = SmtpProperties(),
-            val imapsProperties: ImapsProperties = ImapsProperties(),
+            val port: Int = 587,
+            val properties: MailProperties = MailProperties(),
+            val baseUrl: String = "",
         ) {
-            class SmtpProperties(
+            class MailProperties(
                 val debug: Boolean = false,
-                val smtp: Smtp = Smtp()
+                val transfer: MailTransfer = MailTransfer(),
+                val transport: Transport = Transport(),
             ) {
-                class Smtp(
-                    val auth: Boolean = false,
-                    val starttls: Starttls = Starttls()
-                ) {
-                    class Starttls(val enable: Boolean = false)
-                }
-            }
-
-            class ImapsProperties(
-                val debug: Boolean = false,
-                val imaps: Imaps = Imaps()
-            ) {
-                class Imaps(
+                class Transport(val protocol: String = SMTP)
+                class MailTransfer(
                     val auth: Boolean = false,
                     val starttls: Starttls = Starttls()
                 ) {
